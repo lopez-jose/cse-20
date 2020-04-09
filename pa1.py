3ebea97b9633cd7608594b90afc43814a1b07fe1
Jose Lopez <joselopezbusiness@gmail.com>
2020-04-09 05:37:20 -0700

    Added changes

--- pa1.py
+++ pa1.py
@@ -1,9 +1,14 @@
 import math
+#Here we prompt the user for input
 print("Enter the radius of the sphere:", end = ' ')
+
+#We read that input cast it to a float and store it in radius
 radius = float(input())
 
-
+#These are the equations to find the volume and area, stored respectively
 volume = (4/3)*math.pi*pow(radius,3)
+area = 4*math.pi*pow(radius,2)
 
-print("The volume is: ")
-print(volume)
+#We then print the output in the correct format
+print("The volume is: ",volume)
+print("The surface area is: ", area)
