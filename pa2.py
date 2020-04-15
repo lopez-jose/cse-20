a15c589731cc948372590317a31ddf3d84116939
Jose Lopez <joselopezbusiness@gmail.com>
2020-04-14 20:44:24 -0700

    Added Programming Assignment 2

---
+++ pa2.py
@@ -0,0 +1,17 @@
+import math
+#you will create a python program program that will 
+
+
+def sphere_volume(r):
+    return 0
+
+
+def sphere_area(r):
+    return 0
+
+def cylinder_volume(r,h):
+    return 0
+
+
+def cylinder_area(r,h):
+    return 0
